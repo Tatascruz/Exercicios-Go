a3eb106af03bd4cec42a6853eaef35fc6b897716
Thalita <thalitascruz@icloud.com>
2025-08-25 16:19:40 -0300

    Adiciona variaveis e concatenação em Go

---
+++ Variaveis/variaveisconcanetacao.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nome string = "Tereza"
+	var idade int = 36
+	var altura float64 = 1.65
+
+	fmt.Println("Olá, meu nome é", nome, "tenho", idade, "anos, e", altura, "de altura.")
+}
