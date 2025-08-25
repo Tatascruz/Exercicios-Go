b9f4a2d2c28235e10451e99fd0431f35f57eda09
Thalita <thalitascruz@icloud.com>
2025-08-25 15:47:10 -0300

    Adiciona variaveis simples em Go

---
+++ Variaveis/variaveissimples.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nome string = "Tata"
+	var idade int = 39
+	var cidade string = "Campinas"
+	var gostaDeProgramacao bool = true
+
+	fmt.Println("Meu nome é", nome, ", tenho", idade, "e nasci na cidade de", cidade)
+	fmt.Println("Gosto de programação?", gostaDeProgramacao)
+}
