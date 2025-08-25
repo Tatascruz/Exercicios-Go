623cbcb2d92ea32be2a1b52a2d8f15bec2a4ff03
Thalita <thalitascruz@icloud.com>
2025-08-25 15:55:04 -0300

    Adiciona operações simples em Go

---
+++ Variaveis/operacoessimples.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 1456
+	var b int = 538
+
+	fmt.Println("Soma:", a+b)
+	fmt.Println("Subtração:", a-b)
+	fmt.Println("Multiplicação:", a*b)
+	fmt.Println("Divisão:", a/b)
+}
