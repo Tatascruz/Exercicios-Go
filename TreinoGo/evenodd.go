845fc447e788ee1995455ecfb7b24ee74307559f
Thalita <thalitascruz@icloud.com>
2025-10-09 08:21:31 -0300

    Adiciona exercicio de even ou odd na linguagem Go

---
+++ TreinoGo/evenodd.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func EvenOrOdd(number int) string {
+	if number%2 == 0 {
+		return "Even"
+	} else {
+		return "Odd"
+	}
+}
+
+func main() {
+	fmt.Println(EvenOrOdd(5))
+	fmt.Println(EvenOrOdd(12))
+	fmt.Println(EvenOrOdd(133))
+	fmt.Println(EvenOrOdd(1))
+	fmt.Println(EvenOrOdd(1266))
+}
